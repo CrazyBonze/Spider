035cf7c0e28ff0b245905fff3b5e33e5057fdae5
Michael Taylor <90michaeltaylor@gmail.com>
2016-02-29 20:25:57 -0800

    Now you can create project directory with queue and crawled files.

--- general.py
+++ general.py
@@ -7,4 +7,18 @@ def create_project_dir(directory):
         print('Creating project ' + directory)
         os.makedirs(directory)
 
+#Create queue and crawled files
+def create_data_files(project_name, base_url):
+    queue = project_name + '/queue.txt'
+    crawled = project_name + '/crawled.txt'
+    if not os.path.exists(queue):
+        write_file(queue, base_url)
+    if not os.path.exists(crawled):
+        write_file(crawled, '')
+
+#Creates a new file
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
 
