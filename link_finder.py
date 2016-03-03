5ff69390a6e83de68c019ba5780ccbf78bdc861c
Michael Taylor <90michaeltaylor@gmail.com>
2016-03-02 22:29:20 -0800

    Removed the number one spam feature.

--- link_finder.py
+++ link_finder.py
@@ -15,7 +15,6 @@ class LinkFinder(HTMLParser):
             for (attribute, value) in attrs:
                 if attribute == 'href':
                     url = parse.urljoin(self.base_url, value)
-                    print( url)
                     self.links.add(url)
 
     def page_links(self):
